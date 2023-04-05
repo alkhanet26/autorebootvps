7726db96ff332be693bfe59da5e35f2c0aebfe6c
Alkhanet <92195275+alkhanet26@users.noreply.github.com>
2023-04-05 08:56:57 +0700

    Create auto-reboot.sh

---
+++ auto-reboot.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Auto Reboot VPS
+
+tanggal=$(date +"%m-%d-%Y")
+waktu=$(date +"%T")
+echo "Server telah berhasil reboot pada tanggal $tanggal pukul $waktu." >> /root/log-reboot.txt
+/sbin/shutdown -r now
