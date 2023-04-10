9f4ecf1fca9d546ee60e18282bc79d2236432f62
Alkhanet <92195275+alkhanet26@users.noreply.github.com>
2023-04-11 00:53:14 +0700

    Update auto-reboot.sh

--- auto-reboot.sh
+++ auto-reboot.sh
@@ -4,4 +4,5 @@
 tanggal=$(date +"%m-%d-%Y")
 waktu=$(date +"%T")
 echo "Server telah berhasil reboot pada tanggal $tanggal pukul $waktu." >> /root/log-reboot.txt
-sudo reboot
+/sbin/shutdown -r now
+
