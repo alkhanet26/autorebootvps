2eab8110093e27ed28932104c4ee08d9c603ee1d
Alkhanet <92195275+alkhanet26@users.noreply.github.com>
2023-04-12 00:39:01 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ wget --no-check-certificate "https://raw.githubusercontent.com/alkhanet26/autore
 clear
 echo -e "setting Auto Reboot at 00.00"
 sleep 3
-echo "0 0 * * * root /root/auto-reboot.sh" > /etc/cron.d/auto-reboot
+echo "0 0 * * *  /root/auto-reboot.sh" > /etc/cron.d/auto-reboot
 clear
 echo -e "Auto Reboot VPS installed"
 sleep 3
