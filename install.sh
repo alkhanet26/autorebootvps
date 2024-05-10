023b85bb3eaf6f874fc53a0300458ebba08b6fda
Alkhanet <92195275+alkhanet26@users.noreply.github.com>
2024-05-10 23:20:10 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,8 @@
 
 
 {
+sysctl -w net.ipv6.conf.all.disable_ipv6=1 && sysctl -w net.ipv6.conf.default.disable_ipv6=1
+clear
 echo -e "Install X-UI English"
 bash <(curl -Ls https://raw.githubusercontent.com/MrCenTury/xXx-UI/master/install.sh)
 clear
